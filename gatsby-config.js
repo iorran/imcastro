704162c90253950ae9f35f6a35fa28502df25eee
Iorran <iorran.castro@vakt.io>
2020-07-15 02:09:06 +0100

    (feat) CV changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,24 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Iorran Castro profile`,
+    
+    // Used for the title template on pages other than the index site
+    siteTitle: `Iorran`,
+    // Default title of the page
+    siteTitleAlt: `Iorran Castro profile`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Iorran Castro profile`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `http://imcastro.pt/`,
+    // Used for SEO
+    siteDescription: `Find out a little bit about iorran castro`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@iorranmc`,
   },
   plugins: [
     {
@@ -27,8 +44,8 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        short_name: `Iorran`,
+        description: `Dynamic profile`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
